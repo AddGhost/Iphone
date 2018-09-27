e2f5c34ff849321cb302914f463ff2557d04d9e5
Naveen <naveenbs123@gmail.com>
2018-09-27 14:52:17 +0530

    New changes are added

--- iPhone/src/iPhone/PrintPhoneName.java
+++ iPhone/src/iPhone/PrintPhoneName.java
@@ -5,6 +5,8 @@ public class PrintPhoneName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Iphone");
+
+System.out.println("New changes are added :");
 	}
 
 }
